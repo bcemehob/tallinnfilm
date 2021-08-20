5e541b64a04a69f34cc12453dae161f60852eaa5
Vadim Semenov <vsemenov@leongaming.com>
2021-08-20 10:23:22 +0300

    migrate to java gradle app

---
+++ src/main/java/org/music/tallinnfillm/TallinnfillmApplication.java
@@ -0,0 +1,13 @@
+package org.music.tallinnfillm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TallinnfillmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TallinnfillmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/music/tallinnfillm/TallinnfillmApplicationTests.java
@@ -0,0 +1,13 @@
+package org.music.tallinnfillm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TallinnfillmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
