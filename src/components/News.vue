b2d457e7ae0733765c5e702ae59f5a53c027dcae
Vadim Semenov <vadim.semenov@holmbank.ee>
2023-09-24 23:21:01 +0300

    add new links to release platforms

--- src/components/News.vue
+++ src/components/News.vue
@@ -3,7 +3,7 @@
     <div class="close-box clickable" @click="hideSubpage">x</div>
     <div class="text">
       <div class="subpage-title">News</div>
-      <ul>
+      <ul class="inline-list">
         <li>On Sept 6, 2023 the EP <span class="rus">"В Доме Напротив"</span> (In The Opposite House) has been released by Tallinnfilm.</li>
         <li class="clickable external-link spotify">
           <a href="https://open.spotify.com/album/46PuKuMG6XE5pJwZ390ZmH?si=jS5WqXSGRASjLEW0laGUGQ" target="_blank">Spotify</a>
@@ -13,6 +13,15 @@
         </li>
         <li class="clickable external-link bandcamp">
           <a href="https://tallinnfilm.bandcamp.com/album/-" target="_blank">Bandcamp</a>
+        </li>
+        <li class="clickable external-link apple-music">
+          <a href="https://music.apple.com/us/album/в-доме-напротив-ep/1705938194" target="_blank">Apple Music</a>
+        </li>
+        <li class="clickable external-link amazon-music">
+          <a href="https://music.amazon.com.mx/albums/B0CHBMSSYY" target="_blank">Amazon Music</a>
+        </li>
+        <li class="clickable external-link deezer">
+          <a href="https://www.deezer.com/en/album/484768865" target="_blank">Deezer</a>
         </li>
       </ul>
     </div>
